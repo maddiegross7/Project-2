7d53d414247c3406b3e435b0f90ecba1e419a165
Ava Sangiuliano <ava.sangiuliano@gmail.com>
2024-09-17 09:02:16 -0400

    qsort added

--- qsort.cpp
+++ qsort.cpp
@@ -9,6 +9,26 @@ using namespace std;
 
 void qsort_sort(List &l, bool numeric) {
     cout << "qsort happens here\n";
-    return;
+    vector<struct Node *> list;
+
+   Node *current = l.head->next;
+
+    while(current!=NULL){
+        list.push_back(current);
+        current = current->next;
+    }
+
+    if(numeric){
+       qsort(list.begin(), list.end(), node_number_compare);
+    }else{
+		qsort(list.begin(), list.end(), node_string_compare);
+    }
+
+    l.head->next = *list.begin();
+    for(auto it = list.begin(); it != list.end(); it++){
+        Node* now = *it;
+        now->next = *(it+1);
+    }
+
 }
 
